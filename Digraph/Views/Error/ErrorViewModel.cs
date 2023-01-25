6c24e38d45d84597d9f545028afb77ff2f910e11
unknown <nicolas.skucek@riganti.cz>
2023-01-25 17:41:51 +0100

    Packages updated

--- Digraph/Views/Error/ErrorViewModel.cs
+++ Digraph/Views/Error/ErrorViewModel.cs
@@ -10,10 +10,10 @@ public class ErrorViewModel : DotvvmViewModelBase
     public string? RequestId { get; set; }
 
     [Bind(Direction.None)]
-    public string ExceptionType { get; set; }
+    public string? ExceptionType { get; set; }
 
     [Bind(Direction.None)]
-    public string RequestPath { get; set; }
+    public string? RequestPath { get; set; }
 
 
     public ErrorViewModel()
