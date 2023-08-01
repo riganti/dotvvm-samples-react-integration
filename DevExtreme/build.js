3378eaac915fee95825c007b623a22021c94d8ae
Oleksandr Prokofiev <oleksandr.prokofiev@riganti.cz>
2023-08-01 02:44:42 +0200

    Init commit

---
+++ DevExtreme/build.js
@@ -0,0 +1,25 @@
+﻿import esbuild from 'esbuild'
+
+async function main() {
+
+    try {
+        await esbuild.build({
+            format: 'esm',
+            bundle: true,
+            entryPoints: ["wwwroot/PieChart.tsx"],
+            outdir: `wwwroot`,
+            target: [
+                'es2020'
+            ],
+            sourcemap: true,
+            treeShaking: false,
+            minify: false,
+            mangleProps: /^_/
+        });
+    } catch (err) {
+        console.error(`Cannot build : ${err}`);
+    }
+
+}
+
+main();
---
+++ DevExtreme/wwwroot/PieChart.tsx
@@ -0,0 +1,52 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { registerReactControl } from "dotvvm-jscomponent-react";
+import { PieChart, Series } from 'devextreme-react/pie-chart';
+
+const billionaires = [
+    {
+        country: "China",
+        amount: 1002
+    },
+    {
+        country: "United States",
+        amount: 716
+    },
+    {
+        country: "India",
+        amount: 215
+    },
+    {
+        country: "United Kingdom",
+        amount: 150
+    },
+    {
+        country: "Germany",
+        amount: 145
+    }
+];
+
+function pieChart(props: any) {
+    return (
+        <PieChart
+            dataSource={props.sampleData}
+            type="doughnut"
+        >
+            <Series
+                argumentField="country"
+                valueField="amount"
+            >
+            </Series>
+        </PieChart>
+    );
+}
+
+export default (context: any) => ({
+    $controls: {
+        schedulerComponent: registerReactControl(pieChart,{
+            context,
+            sampleData: billionaires,
+            onClick() { /* do nothing when onClick is not set */ }
+        }),
+    }
+})
